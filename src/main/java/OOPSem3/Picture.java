90c93e7eaf9d57b02295c8fa1bc4324d16e88acb
Frenchfan <sumkin.gpb@gmail.com>
2023-01-20 21:05:05 +0300

    Added Seminar3 - task1

---
+++ src/main/java/OOPSem3/Picture.java
@@ -0,0 +1,18 @@
+package OOPSem3;
+
+public class Picture {
+
+    private int myPicture;
+
+    public Picture(int myPicture) {
+        this.myPicture = myPicture;
+    }
+
+    public int getMyPicture() {
+        return myPicture;
+    }
+
+    public void setMyPicture(int myPicture) {
+        this.myPicture = myPicture;
+    }
+}
