03ad76cbd181addce24b3067e4721e5d0958ff6b
Frenchfan <sumkin.gpb@gmail.com>
2023-01-20 20:03:22 +0300

    Added Homework2 - immutable class

---
+++ src/main/java/OOPSem2/Homework2/Immutable.java
@@ -0,0 +1,33 @@
+package OOPSem2.Homework2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Immutable {
+    private String name;
+
+    private int age;
+
+    Map<String, String> myMap;
+
+    public Immutable(String name, int age, Map<String, String> myMap) {
+        this.name = name;
+        this.age = age;
+        Map<String, String> deepCopy = new HashMap<>();
+        for(String key: myMap.keySet()) {
+            deepCopy.put(key, myMap.get(key));
+        }
+        this.myMap = myMap;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public Map<String, String> getMyMap() {
+        return myMap;
+    }
+}
